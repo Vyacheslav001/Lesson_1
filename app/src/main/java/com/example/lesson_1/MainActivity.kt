bf379a99cc543ed2bab45f71864245df6939760d
Vyacheslav001 <leo-slava@mail.ru>
2022-02-22 16:40:20 +0300

    Поигрались с классами, их свойствами (НЕ ПОЛЯМИ!!!) и функциями (НЕ МЕТОДАМИ!!!)

--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.lesson_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
+    //В Котлине СВОЙСТВА!!:
+    var i: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val objTest = Test()
+        val getI = objTest.i
+        objTest.i = 2
+        this.i = 2
+
+        this.i = try {
+            throw IllegalStateException()
+        }catch (e: IllegalStateException){
+            3
+        }
+
+        Log.d("myLogs", "$i adssfv $i ${i}")
     }
+
+}
+
+class Test(){
+    var i: Int = 0
+}
+
+fun test(): Int{
+    var i: Int = 0
+    return i
 }
---
+++ app/src/main/java/com/example/lesson_1/NewClass.kt
@@ -0,0 +1,7 @@
+package com.example.lesson_1
+
+class NewClass {
+    fun test2(){
+        val i = test()
+    }
+}
